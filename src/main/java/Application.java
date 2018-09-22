caad8ac2926c6d11cf44ebeb9dd79fc35c725b24
Игорь Иваньков <ivankov.i.f@gmail.com>
2018-09-22 13:02:12 +0300

    Enabled swagger2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,12 +3,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"controller", "repository"})
 @EnableJpaRepositories(basePackages = "repository")
 //        entityManagerFactoryRef = "factoryBean")
 @EntityScan(basePackages = "model")
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
